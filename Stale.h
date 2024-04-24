87f8c793220a66c750be60854462441d95fa042a
zuzia <zuzannabrzeczek71@gmail.com>
2024-04-25 00:05:53 +0200

    na laptop

--- Stale.h
+++ Stale.h
@@ -5,23 +5,26 @@
 #ifndef AIDS2_STALE_H
 #define AIDS2_STALE_H
 
+#include <string>
+using namespace std;
+
 #define TRUE 1
 #define FALSE 0
 #define MAXSIZE 11
 
-const char * sizeString = "BOARD_SIZE";
-const char * pNumString = "PAWNS_NUMBER";
-const char * bCorrectString = "IS_BOARD_CORRECT";
-const char * GameOverString = "IS_GAME_OVER";
-const char * bPossibleString = "IS_BOARD_POSSIBLE";
-const char * red1NaiveString = "CAN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
-const char * blue1NaiveString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
-const char * red2NaiveString = "CAN_RED_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
-const char * blue2NaiveString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
-const char * red1PerfectString = "CAN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
-const char * blue1PerfectString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
-const char * red2PerfectString = "CAN_RED_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
-const char * blue2PerfectString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
+string sizeString = "BOARD_SIZE";
+string pNumString = "PAWNS_NUMBER";
+string bCorrectString = "IS_BOARD_CORRECT";
+string GameOverString = "IS_GAME_OVER";
+string bPossibleString = "IS_BOARD_POSSIBLE";
+string red1NaiveString = "CAN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
+string blue1NaiveString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
+string red2NaiveString = "CAN_RED_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
+string blue2NaiveString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
+string red1PerfectString = "CAN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
+string blue1PerfectString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
+string red2PerfectString = "CAN_RED_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
+string blue2PerfectString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
 
 
 #endif //AIDS2_STALE_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <string>
 
 #include "Stale.h"
+
+using namespace std;
 
 int main() {
     char ch;
     int nastepnaLinia = FALSE;
+    int input = TRUE;
     int inputPlansza = TRUE;
+    int inputPolecenia = FALSE;
+    string polecenie;
     int iloscB = 0, iloscR = 0;
 
     int iloscIinii = 0;
@@ -23,50 +29,75 @@ int main() {
 
     //wypelnianie planszy na tablice 2d
 
-    while (inputPlansza){
-        ch = std::cin.get();
+    while (input) {
+        if (inputPlansza) {
+            if (ch = cin.get()) {
 
-        if (ch == '-'){
-            if (nastepnaLinia) nastepnaLinia = FALSE;
+                if (ch == '-') {
+                    if (nastepnaLinia) nastepnaLinia = FALSE;
+                } else if (ch == '<') {
+                    if (nastepnaLinia) {
+                        iloscIinii = zmiennaLinii;
+                        nastepnaLinia = FALSE;
+                    }
+                    kolumna++;
+
+                } else if (ch == 'b') {
+                    iloscB++;
+                    hexWTablicy[kolumna - 1][zmiennaLinii] = 'b';
+                } else if (ch == 'r') {
+                    iloscR++;
+                    hexWTablicy[kolumna - 1][zmiennaLinii] = 'r';
+                } else if (ch == '\n') {
+                    kolumna = 0;
+                    zmiennaLinii++;
+                    nastepnaLinia = TRUE;
+                } else if (ch == '>') {
+                    if (hexWTablicy[kolumna - 1][zmiennaLinii] != 'r' &&
+                        hexWTablicy[kolumna - 1][zmiennaLinii] != 'b')
+                        hexWTablicy[kolumna - 1][zmiennaLinii] = 'n';
+                }
+
+                if (iloscIinii != 0 && iloscIinii * 2 + 1 == zmiennaLinii) {
+                    inputPlansza = FALSE;
+                    inputPolecenia = TRUE;
+                }
+            }
+            else break;
         }
-        else if (ch == '<'){
-            if (nastepnaLinia){
-                iloscIinii = zmiennaLinii;
+
+        if (inputPolecenia) {
+            getline (cin,polecenie);
+            if(cin.eof()) break;
+
+            if (polecenie == sizeString){
+                cout << iloscIinii << endl << endl;
+            }
+            else if (polecenie == pNumString){
+                cout << iloscB + iloscR << std::endl << std::endl;
+            }
+            else if (polecenie == bCorrectString){
+
+            }
+            else {
+                inputPolecenia = FALSE;
+                inputPlansza = TRUE;
+                iloscIinii = 0;
+                kolumna = 0;
+                zmiennaLinii = 0;
+                iloscB = 0;
+                iloscR = 0;
                 nastepnaLinia = FALSE;
             }
-            kolumna++;
-
-        }
-        else if (ch == 'b'){
-            iloscB ++ ;
-            hexWTablicy[kolumna-1][zmiennaLinii] = 'b';
-        }
-        else if (ch == 'r') {
-            iloscR ++ ;
-            hexWTablicy[kolumna-1][zmiennaLinii] = 'r';
-        }
-        else if (ch == '\n') {
-            kolumna = 0;
-            zmiennaLinii++;
-            nastepnaLinia = TRUE;
-        }
-        else if (ch == '>'){
-            if (hexWTablicy[kolumna-1][zmiennaLinii] != 'r' && hexWTablicy[kolumna-1][zmiennaLinii] != 'b') hexWTablicy[kolumna-1][zmiennaLinii] = 'n';
         }
 
-        if (iloscIinii != 0 && iloscIinii*2+1 == zmiennaLinii) inputPlansza = FALSE;
+        /*for (int i = 0; i < MAXSIZE; i++) {
+            for (int j = 0; j < MAXSIZE; j++) {
+                std::cout << hexWTablicy[j][i] << ' ';
+            }
+            std::cout << std::endl;
+        }*/
     }
-
-    std::cout << iloscIinii << std::endl << std::endl;
-    std::cout << iloscB+iloscR << std::endl << std::endl;
-
-    for (int i = 0; i < MAXSIZE; i++){
-        for (int j = 0; j < MAXSIZE; j++){
-            std::cout << hexWTablicy[j][i] << ' ';
-        }
-        std::cout << std::endl;
-    }
-
     return 0;
 }
 
