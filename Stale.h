3f8845f0bc0b670f5435da95171d5876755b8a06
Zuzanna Brzęczek <zuzannabrzeczek71@gmail.com>
2024-04-29 14:20:50 +0200

    jakims cudem 64%

--- Stale.h
+++ Stale.h
@@ -8,23 +8,28 @@
 #include <string>
 using namespace std;
 
+struct Point{
+    int x;
+    int y;
+};
+
 #define MAXSIZE 11
 #define BLUE 2
 #define RED 1
 
-string sizeString = "BOARD_SIZE";
-string pNumString = "PAWNS_NUMBER";
-string bCorrectString = "IS_BOARD_CORRECT";
-string gameOverString = "IS_GAME_OVER";
-string bPossibleString = "IS_BOARD_POSSIBLE";
-string red1NaiveString = "CAN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
-string blue1NaiveString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
-string red2NaiveString = "CAN_RED_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
-string blue2NaiveString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
-string red1PerfectString = "CAN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
-string blue1PerfectString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
-string red2PerfectString = "CAN_RED_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
-string blue2PerfectString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
+const string sizeString = "BOARD_SIZE";
+const string pNumString = "PAWNS_NUMBER";
+const string bCorrectString = "IS_BOARD_CORRECT";
+const string gameOverString = "IS_GAME_OVER";
+const string bPossibleString = "IS_BOARD_POSSIBLE";
+const string red1NaiveString = "CAN_RED_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
+const string blue1NaiveString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_NAIVE_OPPONENT";
+const string red2NaiveString = "CAN_RED_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
+const string blue2NaiveString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_NAIVE_OPPONENT";
+const string red1PerfectString = "CAN_RED_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
+const string blue1PerfectString = "CAN_BLUE_WIN_IN_1_MOVE_WITH_PERFECT_OPPONENT";
+const string red2PerfectString = "CAN_RED_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
+const string blue2PerfectString = "CAN_BLUE_WIN_IN_2_MOVES_WITH_PERFECT_OPPONENT";
 
 
 #endif //AIDS2_STALE_H
--- main.cpp
+++ main.cpp
@@ -1,34 +1,146 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 #include "Stale.h"
 
 using namespace std;
 
-int isGameOverFunction(char tablica[MAXSIZE][MAXSIZE], int iloscLinii){
-    int isGameOver;
+int isGameOverFunction(char tablicaKonc[MAXSIZE][MAXSIZE], int iloscLinii){
+    char tablica[MAXSIZE][MAXSIZE];
 
-    //najpeirw czerwone jest sprawdzane
-    for (int i  =0; i < iloscLinii; i ++){
-        if (tablica[0][i] == 'r') {
-            
+    for (int i = 0; i<iloscLinii; i++){
+        for (int j = 0; j<iloscLinii; j++){
+            tablica[i][j] = tablicaKonc[i][j];
         }
     }
 
+    if (iloscLinii == 1){
+        if (tablica[0][0] == 'b') return BLUE;
+        else if (tablica[0][0] == 'r') return RED;
+        else return false;
+    }
 
-    return isGameOver;
+
+    stack<Point> stosPktRed, stosPktBlue;
+    int zmiennaX, zmiennaY;
+    char sprawdzanie;
+    stack<Point>  *stosPkt;
+
+
+    Point pozycjaRed;
+    Point pozycjaBlue;
+    Point pozycja;
+    //najpeirw czerwone jest sprawdzane
+    for (int i = 0; i < iloscLinii; i ++){
+
+        //sprawdzenie dolnej krawedzi dla czerwonego
+        if (tablica[iloscLinii-1][i] == 'r') {
+            tablica[iloscLinii-1][i] = 'N';
+            if (tablica[iloscLinii-2][i] == 'r'){
+                pozycjaRed.x = i;
+                pozycjaRed.y = iloscLinii-2;
+                stosPktRed.push(pozycjaRed);
+            }
+            else if (i+1 < iloscLinii && tablica[iloscLinii-2][i+1] == 'r'){
+                pozycjaRed.x = i+1;
+                pozycjaRed.y = iloscLinii-2;
+                stosPktRed.push(pozycjaRed);
+            }
+        }
+
+        //sprawdzenie bocznej krawedzi dla niebieskiego
+        if (tablica[i][0] == 'b') {
+            tablica[i][0] = 'N';
+            if (i-1 >= 0 && tablica[i-1][1] == 'b'){
+                pozycjaBlue.x = 1;
+                pozycjaBlue.y = i-1;
+                stosPktBlue.push(pozycjaBlue);
+            }
+            else if (tablica[i][1] == 'b'){
+                pozycjaBlue.x = 1;
+                pozycjaBlue.y = i;
+                stosPktBlue.push(pozycjaBlue);
+            }
+        }
+    }
+
+    while (!stosPktRed.empty() || !stosPktBlue.empty()){
+        if(!stosPktRed.empty()) {
+            pozycja = stosPktRed.top();
+            if (pozycja.y == 0) {
+                return RED;
+            }
+            stosPktRed.pop();
+            tablica[pozycja.y][pozycja.x] = 'N';
+            sprawdzanie = 'r';
+            stosPkt = &stosPktRed;
+            zmiennaY = 0;
+            zmiennaX = 0;
+        }
+        else if (!stosPktBlue.empty()){
+            pozycja = stosPktBlue.top();
+            if (pozycja.x == iloscLinii-1) {
+                return BLUE;
+            }
+            stosPktBlue.pop();
+            tablica[pozycja.y][pozycja.x] = 'N';
+            sprawdzanie = 'b';
+            stosPkt = &stosPktBlue;
+            zmiennaY = 1;
+            zmiennaX = 1;
+        }
+
+        Point przetrzymujaca;
+
+        //musisz dac jakas zmienna na y zeby nie zmieniac za kazdym raze
+        // jezeli stos jest pusty a nie drugi koniec to falsz
+
+        if (pozycja.x + zmiennaX < iloscLinii && pozycja.y - 1 + zmiennaY >= 0 && tablica[pozycja.y - 1 + zmiennaY][pozycja.x + zmiennaX] == sprawdzanie){
+            przetrzymujaca.x = pozycja.x + zmiennaX;
+            przetrzymujaca.y = pozycja.y - 1 + zmiennaY;
+            stosPkt->push(przetrzymujaca);
+        }
+        if (pozycja.y - 1 >= 0 && pozycja.x + 1 < iloscLinii && tablica[pozycja.y - 1][pozycja.x + 1] == sprawdzanie){
+            przetrzymujaca.x = pozycja.x + 1;
+            przetrzymujaca.y = pozycja.y - 1;
+            stosPkt->push(przetrzymujaca);
+        }
+        if (pozycja.x + 1 - zmiennaX < iloscLinii  && pozycja.y + zmiennaY  < iloscLinii &&  tablica[pozycja.y + zmiennaY][pozycja.x + 1 - zmiennaX] == sprawdzanie){
+            przetrzymujaca.x = pozycja.x + 1 - zmiennaX;
+            przetrzymujaca.y = pozycja.y + zmiennaY;
+            stosPkt->push(przetrzymujaca);
+        }
+        if (pozycja.x - 1 + zmiennaX >= 0 && pozycja.y - zmiennaY >= 0 && tablica[pozycja.y - zmiennaY][pozycja.x - 1 + zmiennaX] == sprawdzanie){
+            przetrzymujaca.x = pozycja.x - 1 + zmiennaX;
+            przetrzymujaca.y = pozycja.y - zmiennaY;
+            stosPkt->push(przetrzymujaca);
+        }
+        if (pozycja.y + 1 < iloscLinii && pozycja.x - zmiennaX >= 0 && tablica[pozycja.y + 1][pozycja.x - zmiennaX] == sprawdzanie){
+            przetrzymujaca.x = pozycja.x - zmiennaX;
+            przetrzymujaca.y = pozycja.y + 1;
+            stosPkt->push(przetrzymujaca);
+        }
+        if (pozycja.x - 1 >= 0 && pozycja.y + 1 - zmiennaY <iloscLinii && tablica[pozycja.y + 1 - zmiennaY][pozycja.x - 1] == sprawdzanie){
+            przetrzymujaca.x = pozycja.x - 1;
+            przetrzymujaca.y = pozycja.y + 1 - zmiennaY;
+            stosPkt->push(przetrzymujaca);
+        }
+
+    }
+
+    return false;
 }
 
 int main() {
-    char ch;
-    bool nastepnaLinia = false;
+    char ch, zmienna;
     bool inputPlansza = true;
     bool inputPolecenia = false;
     bool koniecRzedow = false;
     bool isBoardCorrect = false;
-    int isGameOver;
+    int isGameOver, iloscPionkowZmienna;
     string polecenie;
-    int iloscB = 0, iloscR = 0;
+    int iloscB = 0, iloscR = 0, zmiennaIloscWygranychPossible = 0;
 
     int iloscIinii = 0;
     int kolumna = 0;
@@ -58,15 +170,15 @@ int main() {
             inputPlansza = false;
             inputPolecenia = true;
             polecenie = "";
-            if (iloscB == iloscR || iloscR == iloscB +1) isBoardCorrect = true;
+            if (iloscB == iloscR || iloscR == iloscB + 1) isBoardCorrect = true;
 
             //stworzenie koncowej tablicy
             for (int i = 0; i < 2 * MAXSIZE -1; i++){
                 for (int j = 0; j < 2 * MAXSIZE -1; j++){
                     if (hexWTablicy[i][j] != ' ') {
-                        if (i < iloscIinii) {
+                        if (i < iloscIinii && j < MAXSIZE) {
                             tablicaKonc[j][iloscIinii - 1 - i + j] = hexWTablicy[i][j];
-                        } else {
+                        } else if (j < MAXSIZE){
                             tablicaKonc[i - iloscIinii + 1 + j][j] = hexWTablicy[i][j];
                         }
                     }
@@ -74,18 +186,7 @@ int main() {
             }
 
             isGameOver = isGameOverFunction(tablicaKonc, iloscIinii);
-            /*for (int i = 0; i < MAXSIZE * 2 - 1; i++){
-                for (int j = 0; j < MAXSIZE * 2 -1 ; j++){
-                    cout << hexWTablicy[i][j] << ' ';
-                }
-                cout << endl;
-            }
-            for (int i = 0; i < iloscIinii; i++){
-                for (int j = 0; j < iloscIinii; j++){
-                    cout << tablicaKonc[i][j] << ' ';
-                }
-                cout << endl;
-            }*/
+
         }
 
         if (ch == '-' && inputPolecenia) {
@@ -96,9 +197,20 @@ int main() {
             zmiennaLinii = 0;
             iloscB = 0;
             iloscR = 0;
-            nastepnaLinia = false;
             zmiennaKresek = 0;
             koniecRzedow = false;
+            isBoardCorrect = false;
+            zmiennaIloscWygranychPossible = 0;
+            for (int i = 0; i < MAXSIZE * 2 - 1; i++){
+                for (int j = 0; j < MAXSIZE * 2 - 1; j++){
+                    hexWTablicy[j][i] = ' ';
+                }
+            }
+            for (int i = 0; i < MAXSIZE; i++){
+                for (int j = 0; j < MAXSIZE; j++){
+                    tablicaKonc[j][i] = ' ';
+                }
+            }
         }
 
         //input plansyz i danych
@@ -155,7 +267,7 @@ int main() {
             }
             else if (polecenie == gameOverString){
                 if (isBoardCorrect){
-                    if (isGameOver == false) {
+                    if (!isGameOver) {
                         cout << "NO" << endl;
                     }
                     else if (isGameOver == BLUE) {
@@ -169,8 +281,32 @@ int main() {
                     cout << "NO" << endl;
                 }
             }
+            else if (polecenie == bPossibleString){
+                if (isBoardCorrect){
+                    if (isGameOver > 0){
+                        if ((isGameOver == BLUE && iloscB==iloscR) || (isGameOver== RED && iloscR-1 == iloscB)){
+                            zmienna = (isGameOver == BLUE) ? 'b' : 'r';
+                            iloscPionkowZmienna = (isGameOver == BLUE) ? iloscB : iloscR;
+                            for (int i =0; i < iloscIinii; i++){
+                                for (int j =0; j < iloscIinii; j++){
+                                    if (tablicaKonc[i][j] == zmienna){
+                                        tablicaKonc[i][j] = 'N';
+                                        isGameOver = isGameOverFunction(tablicaKonc, iloscIinii);
+                                        if (isGameOver > 0) zmiennaIloscWygranychPossible++;
+                                        tablicaKonc[i][j] = zmienna;
+                                    }
+                                }
+                            }
+                            if (zmiennaIloscWygranychPossible == iloscPionkowZmienna) cout << "NO" << endl;
+                            else cout << "YES" << endl;
+                        }
+                        else cout << "NO" << endl;
+                    }
+                    else cout << "YES" << endl;
+                }
+                else cout << "NO" << endl;
+            }
         }
-
     }
     return 0;
 }
